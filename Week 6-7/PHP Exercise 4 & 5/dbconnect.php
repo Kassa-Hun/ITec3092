e7f78a71f5c508fd52edc23608146eb55039b63e
Kassa-Hun <56105675+Kassa-Hun@users.noreply.github.com>
2021-04-22 12:46:38 +0300

    Delete Week 6-7/PHP Exercise 4 & 5 directory

--- Week 6-7/PHP Exercise 4 & 5/dbconnect.php
+++
@@ -1,9 +0,0 @@
-<?php
-	define("HOSTNAME", "localhost");
-	define("USERNAME", "root");
-	define("PASSWORD", "");
-	define("DBNAME", "dmu");
-	
-	$connection=mysqli_connect(HOSTNAME,USERNAME,PASSWORD,DBNAME);
-	
- ?>
--- Week 6-7/PHP Exercise 4 & 5/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
- session_start();
- if(isset($_SESSION['u'],$_SESSION['p'],$_SESSION['r']))
-	unset($_SESSION['u'],$_SESSION['p'],$_SESSION['r']);
- session_destroy();
- header('Location: index.php');
-?>
